0d74c8094c5439cb7e560e30998a680346e9af53
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-11-07 23:50:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+	res.send('Assignment Eleven Server is Running  check');
+});
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y0hhy5e.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+	const collection = client.db('test').collection('devices');
+	// perform actions on the collection object
+	client.close();
+});
+
+
+
+app.listen(port, () => {
+	console.log(`Assignment Eleven Server Listening on Port ${port}`);
+});

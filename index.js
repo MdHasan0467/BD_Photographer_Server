bbb489d2683ebf22bfe819ca5459fe2b2346e859
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-11-10 23:46:23 +0600

    recheck

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -13,12 +14,29 @@ app.get('/', (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y0hhy5e.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
+
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 });
+
+//! JWT verify ....
+
+// function verifyJWT(req, res, next) {
+// 	const authHeader = req.headers.authorization;
+// 	if (!authHeader) {
+// 		return res.status(401).send({ message: 'unauthorized access' });
+// 	}
+// 	const token = authHeader.split(' ')[1];
+// 	jwt.verify(token, process.env.ACCESS_JWT_TOKEN, function (err, decoded) {
+// 		if (err) {
+// 			return res.status(401).send({ message: 'unauthorized access' });
+// 		}
+// 		req.decoded = decoded;
+// 		next();
+// 	});
+// }
 
 async function run() {
 	try {
@@ -26,6 +44,15 @@ async function run() {
 			.db('photoCollection')
 			.collection('services');
 		const reviewCollection = client.db('photoCollection').collection('reviews');
+
+		//! receive the request from the client side and give him a JWT token...
+		app.post('/jwt', (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_JWT_TOKEN, {
+				expiresIn: '5',
+			});
+			res.send({ token });
+		});
 
 		//! All services are available...
 		app.get('/services', async (req, res) => {
@@ -46,13 +73,11 @@ async function run() {
 		//! three services collection...
 		app.get('/service', async (req, res) => {
 			const query = {};
-
 			const count = await servicesCollection.estimatedDocumentCount(query);
 			const cursor = servicesCollection.find(query);
 			const services = await cursor.skip(parseInt(count) - 3).toArray();
 			res.send(services);
 		});
-
 
 		
 
@@ -78,8 +103,10 @@ async function run() {
 			res.send(result);
 		});
 
-		//! Individually users Review ....
+		//! Individually users Review By checking email ....
 		app.get('/reviews', async (req, res) => {
+			
+
 			let query = {};
 			if (req.query.email) {
 				query = { email: req.query.email };
@@ -106,39 +133,43 @@ async function run() {
 			res.send(result);
 		});
 
-		// //! Put for update
-		// app.put('/update/:id', async (req, res) => {
-		// 	const id = req.params.id;
-		// 	const query = { _id: ObjectId(id) };
-		// 	const updatedReview = req.body;
-		// 	console.log(updatedReview);
-		// });
+		//!!=========>
 
-		// // ! This is CRUD => U = Create //TODO: Update root
-		// app.get('/users/:id', async (req, res) => {
-		// 	const id = req.params.id;
-		// 	const query = { _id: ObjectId(id) }; //! kon id k delete korte chai
-		// 	const result = await userCollection.findOne(query);
-		// 	res.send(result);
-		// });
+		app.patch('/reviews/:id', async (req, res) => {
+			// console.log(req.params.id);
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+		
+			const review = req.body;
+			// console.log(review);
+			const result = await reviewCollection.updateOne(query,{$set:{message:review.message}});
+			res.send(result);
 
-		// //! Update value by Put/patch
-		// app.put('/users/:id', async (req, res) => {
-		// 	const id = req.params.id;
-		// 	const query = { _id: ObjectId(id) }; //! kon id k delete korte chai
-		// 	const user = req.body;
-		// 	const option = { upsert: true }; //! Value thakle Update korbe.. R na thakele Insert korbe
-		// 	const updatedUser = {
-		// 		$set: {
-		// 			name: user.name,
-		// 			email: user.email,
-		// 			address: user.address,
-		// 		},
-		// 	};
-		// 	const result = await userCollection.updateOne(query, updatedUser, option);
-		// 	res.send(result);
-		// 	// console.log(updatedUser)
-		// });
+
+
+
+
+
+
+			// const userEmail = req.query.email;
+			// const updatedId = req.query.id;
+			// console.log(userEmail, updatedId);
+
+			// const decoded = req.decoded.email;
+			// if (decoded !== userEmail) {
+			// 	res.status(403).send({ message: 'Forbidden access' });
+			// }
+			// const currentReview = req.body.status;
+			// console.log(currentReview);
+			// const query = { _id: ObjectId(updatedId) };
+			// const updateDoc = {
+			// 	$set: {
+			// 		message: currentReview,
+			// 	},
+			// };
+			// const result = await reviewCollection.updateOne(query, updateDoc);
+			// res.send(result);
+		});
 	} finally {
 	}
 }
